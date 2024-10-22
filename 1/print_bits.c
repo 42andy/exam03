a045bdaf5e62a91f19150acf9030a7ac99bac148
andy <astoll@student.42lausanne.ch>
2024-10-22 10:40:19 +0200

    ok

--- 1/print_bits.c
+++ 1/print_bits.c
@@ -1,1 +1,14 @@
-void	print_bits(unsigned char octet)
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
+{
+	int i;
+	unsigned char 	bit;
+
+	i = 8;
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
